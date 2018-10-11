83b96ea969b2b7e4a737222c1aac3b4f2329d369
dyb_mi <dybnuaa@163.com>
2018-10-11 13:48:31 +0800

    设置OpenGL3.3核心版本

--- 01EmptyOpenGLWidget/main.cpp
+++ 01EmptyOpenGLWidget/main.cpp
@@ -1,9 +1,15 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSurfaceFormat format;
+    format.setVersion(3, 3);
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(format);
+
     Widget w;
     w.show();
 
